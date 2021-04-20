b53ab82834efffb5e38ca5d742e3ca03c7c4b007
Bernardo Castro <bernardoc544@gmail.com>
2021-04-20 15:12:26 -0400

    Player Boundaries Established

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,9 +19,27 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-        /*vector 3 direction = new Vector3(horizontalInput, verticalInput, 0)
-         transform.Translate(direction * _speed * Time.deltaTime);*/
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if(transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if(transform.position.x >= 11f)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, 0);
+        }
+        else if(transform.position.x <= -11f)
+        {
+            transform.position = new Vector3(11f, transform.position.y, 0);
+        }
     }
 }
