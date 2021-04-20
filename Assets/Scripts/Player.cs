be1ea21990cab8ba452a317f0cbab7bc31fb3b51
Bernardo Castro <bernardoc544@gmail.com>
2021-04-20 16:33:48 -0400

    Shoot Laser Finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
-
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,10 +23,14 @@ public class Player : MonoBehaviour
     {
         Tracker();
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Shooter();
         }
+       
+       
     }
 
     void Tracker()
@@ -49,5 +55,10 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.5f, transform.position.y, 0);
         }
+    }
+    void Shooter()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 }
