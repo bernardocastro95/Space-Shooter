4ac3022c14311046a99bb4e9c347bf10ded413c8
Bernardo Castro <bernardoc544@gmail.com>
2021-04-20 23:18:49 -0400

    Initial Polish

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -5f)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 8, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+        else if(other.tag == "Player")
+        {    
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+               
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,20 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _fireRate = 0.5f;
     private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+    private SpawnManager _spawnManager;
     
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("Spawn Manager NULL");
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +38,8 @@ public class Player : MonoBehaviour
         {
             Shooter();
         }
+
+
        
        
     }
@@ -59,6 +70,16 @@ public class Player : MonoBehaviour
     void Shooter()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.0f, 0), Quaternion.identity);
+    }
+    public void Damage()
+    {
+        _lives--;
+
+        if(_lives < 1)
+        {
+            _spawnManager.onPlayerDeath();
+            Destroy(this.gameObject);
+        }
     }
 }
